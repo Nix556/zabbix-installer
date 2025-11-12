15fb5f80a2d865434f2a1227529fb70b33cf1063
Jonathan JJ <79708886+Nix556@users.noreply.github.com>
2025-11-12 18:19:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,34 +4,36 @@ source lib/utils.sh
 source lib/system.sh
 source lib/db.sh
 
-initialCheck
+detect_os
+update_system
 
-echo -e "${YELLOW}Select installation type:${NC}"
-echo "1) Full Zabbix Server + Agent"
-echo "2) Zabbix Agent only"
-echo "3) Exit"
-read -rp "Choice [1-3]: " INSTALL_CHOICE
+DB_NAME=$(ask "Enter Zabbix DB name: " "zabbix")
+DB_USER=$(ask "Enter Zabbix DB user: " "zabbix")
+DB_PASS=$(ask "Enter Zabbix DB password: " "zabbix123")
+ROOT_PASS=$(ask "Enter MariaDB root password: " "")
+ZABBIX_IP=$(ask "Enter Zabbix Server IP: " "127.0.0.1")
 
-case "$INSTALL_CHOICE" in
-1)
-    installDeps
-    setupMariaDB
-    installApachePHP
-    installZabbixServer
-    installZabbixAgent
-    configureZabbixAPI
-    ;;
-2)
-    installDeps
-    installZabbixAgent
-    ;;
-3)
-    exit 0
-    ;;
-*)
-    echo -e "${RED}Invalid choice${NC}"
-    exit 1
-    ;;
-esac
+info "Installing required packages..."
+sudo $PM install -y wget curl gnupg2 software-properties-common lsb-release \
+apache2 mariadb-server mariadb-client php8.2 php8.2-mysql php8.2-gd php8.2-bcmath php8.2-mbstring php8.2-xml php8.2-ldap php8.2-curl
 
-echo -e "${GREEN}Installation completed.${NC}"
+info "Adding Zabbix repository..."
+wget https://repo.zabbix.com/zabbix/7.4/${OS}/$(lsb_release -cs)/amd64/zabbix-release_7.4-1+${OS}$(lsb_release -rs)_all.deb
+sudo dpkg -i zabbix-release_7.4-1+${OS}$(lsb_release -rs)_all.deb
+sudo $PM update -y
+
+info "Installing Zabbix server, frontend, agent..."
+sudo $PM install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
+
+create_zabbix_db "$DB_NAME" "$DB_USER" "$DB_PASS" "$ROOT_PASS"
+
+info "Importing initial schema..."
+zcat /usr/share/doc/zabbix-sql-scripts/mysql/create.sql.gz | mysql -u"$DB_USER" -p"$DB_PASS" "$DB_NAME"
+
+sudo sed -i "s/^DBName=.*/DBName=$DB_NAME/" /etc/zabbix/zabbix_server.conf
+sudo sed -i "s/^DBUser=.*/DBUser=$DB_USER/" /etc/zabbix/zabbix_server.conf
+sudo sed -i "s/^# DBPassword=.*/DBPassword=$DB_PASS/" /etc/zabbix/zabbix_server.conf
+
+sudo systemctl enable --now zabbix-server zabbix-agent apache2
+
+success "Zabbix installation complete! Access frontend at http://$ZABBIX_IP/zabbix"
