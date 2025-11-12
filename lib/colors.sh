124b1f2f5515e7f263fffafae31276a23657000e
Jonathan JJ <79708886+Nix556@users.noreply.github.com>
2025-11-12 18:17:02 +0100

    Update colors.sh

--- lib/colors.sh
+++ lib/colors.sh
@@ -1,4 +1,11 @@
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m'
+#!/bin/bash
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[0;34m"
+RESET="\033[0m"
+
+info() { echo -e "${BLUE}[INFO]${RESET} $1"; }
+success() { echo -e "${GREEN}[OK]${RESET} $1"; }
+warning() { echo -e "${YELLOW}[WARN]${RESET} $1"; }
+error() { echo -e "${RED}[ERROR]${RESET} $1"; }
