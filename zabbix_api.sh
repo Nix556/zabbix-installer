f7f5e971b4eb1249fceb0b81abc4442cff1a93d4
Jonathan JJ <79708886+Nix556@users.noreply.github.com>
2025-11-12 18:19:43 +0100

    Update zabbix_api.sh

--- zabbix_api.sh
+++ zabbix_api.sh
@@ -1,63 +1,39 @@
 #!/bin/bash
-source lib/colors.sh
-source lib/utils.sh
-
 CONFIG_FILE="config/zabbix_api.conf"
+source lib/colors.sh
 
-loadToken() {
-    [[ -f "$CONFIG_FILE" ]] && source "$CONFIG_FILE"
-}
+if [[ ! -f $CONFIG_FILE ]]; then
+    error "Config file not found. Copy and edit config/zabbix_api.conf"
+    exit 1
+fi
 
-saveToken() {
-    echo "ZBX_TOKEN=$ZBX_TOKEN" >"$CONFIG_FILE"
-}
+source "$CONFIG_FILE"
 
-getToken() {
-    loadToken
-    if [[ -z "$ZBX_TOKEN" ]]; then
-        read -rp "Zabbix URL: " ZBX_URL
-        read -rp "Zabbix User: " ZBX_USER
-        read -rsp "Zabbix Password: " ZBX_PASS
-        echo
-        ZBX_TOKEN=$(curl -s -X POST -H 'Content-Type: application/json' -d \
-        "{\"jsonrpc\":\"2.0\",\"method\":\"user.login\",\"params\":{\"user\":\"$ZBX_USER\",\"password\":\"$ZBX_PASS\"},\"id\":1}" "$ZBX_URL" | jq -r '.result')
-        saveToken
-    fi
-}
+API_JSON='{"jsonrpc":"2.0","method":"user.login","params":{"user":"'"$ZABBIX_USER"'","password":"'"$ZABBIX_PASS"'"},"id":1}'
 
-addHost() {
-    getToken
-    read -rp "Host name: " HOSTNAME
-    read -rp "Host IP: " HOSTIP
-    curl -s -X POST -H 'Content-Type: application/json' -d \
-    "{\"jsonrpc\":\"2.0\",\"method\":\"host.create\",\"params\":{\"host\":\"$HOSTNAME\",\"interfaces\":[{\"type\":1,\"main\":1,\"useip\":1,\"ip\":\"$HOSTIP\",\"dns\":\"\",\"port\":\"10050\"}]},\"auth\":\"$ZBX_TOKEN\",\"id\":1}" "$ZBX_URL"
-    echo -e "${GREEN}Host added.${NC}"
-}
+AUTH_TOKEN=$(curl -s -X POST -H 'Content-Type: application/json' -d "$API_JSON" "$ZABBIX_URL/api_jsonrpc.php" | jq -r '.result')
 
-listHosts() {
-    getToken
-    curl -s -X POST -H 'Content-Type: application/json' -d \
-    "{\"jsonrpc\":\"2.0\",\"method\":\"host.get\",\"params\":{\"output\":[\"hostid\",\"host\"]},\"auth\":\"$ZBX_TOKEN\",\"id\":1}" "$ZBX_URL" | jq -r '.result[] | "\(.hostid) \(.host)"'
-}
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 [list|add|remove] args..."
+    exit 1
+fi
 
-removeHost() {
-    getToken
-    listHosts
-    read -rp "Enter HostID to remove: " HOSTID
-    curl -s -X POST -H 'Content-Type: application/json' -d \
-    "{\"jsonrpc\":\"2.0\",\"method\":\"host.delete\",\"params\":[\"$HOSTID\"],\"auth\":\"$ZBX_TOKEN\",\"id\":1}" "$ZBX_URL"
-    echo -e "${GREEN}Host removed.${NC}"
-}
+ACTION="$1"; shift
 
-echo "1) Add Host"
-echo "2) List Hosts"
-echo "3) Remove Host"
-echo "4) Exit"
-read -rp "Choice [1-4]: " API_CHOICE
-case "$API_CHOICE" in
-1) addHost ;;
-2) listHosts ;;
-3) removeHost ;;
-4) exit 0 ;;
-*) echo -e "${RED}Invalid choice${NC}" ;;
+case "$ACTION" in
+    list)
+        curl -s -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"host.get","params":{"output":["hostid","host","status"]},"auth":"'"$AUTH_TOKEN"'","id":1}' "$ZABBIX_URL/api_jsonrpc.php" | jq
+        ;;
+    add)
+        HOSTNAME="$1"
+        IP="$2"
+        curl -s -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"host.create","params":{"host":"'"$HOSTNAME"'","interfaces":[{"type":1,"main":1,"useip":1,"ip":"'"$IP"'","dns":"","port":"10050"}],"groups":[{"groupid":"2"}]},"auth":"'"$AUTH_TOKEN"'","id":1}' "$ZABBIX_URL/api_jsonrpc.php" | jq
+        ;;
+    remove)
+        HOSTID="$1"
+        curl -s -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"host.delete","params":["'"$HOSTID"'"],"auth":"'"$AUTH_TOKEN"'","id":1}' "$ZABBIX_URL/api_jsonrpc.php" | jq
+        ;;
+    *)
+        error "Unknown action"
+        ;;
 esac
