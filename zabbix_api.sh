a0884ef3a17ccdc5c33d56dad5dc202cf32f1dfd
Jonathan JJ <79708886+Nix556@users.noreply.github.com>
2025-11-12 18:42:11 +0100

    Update zabbix_api.sh

--- zabbix_api.sh
+++ zabbix_api.sh
@@ -1,39 +1,19 @@
 #!/bin/bash
 CONFIG_FILE="config/zabbix_api.conf"
-source lib/colors.sh
-
 if [[ ! -f $CONFIG_FILE ]]; then
-    error "Config file not found. Copy and edit config/zabbix_api.conf"
+    echo "API config not found! Run install.sh first."
     exit 1
 fi
-
 source "$CONFIG_FILE"
 
-API_JSON='{"jsonrpc":"2.0","method":"user.login","params":{"user":"'"$ZABBIX_USER"'","password":"'"$ZABBIX_PASS"'"},"id":1}'
+API_LOGIN=$(curl -s -X POST -H 'Content-Type: application/json-rpc' \
+    -d "{\"jsonrpc\":\"2.0\",\"method\":\"user.login\",\"params\":{\"user\":\"$ZABBIX_USER\",\"password\":\"$ZABBIX_PASS\"},\"id\":1}" \
+    "$ZABBIX_URL/api_jsonrpc.php" | jq -r '.result')
 
-AUTH_TOKEN=$(curl -s -X POST -H 'Content-Type: application/json' -d "$API_JSON" "$ZABBIX_URL/api_jsonrpc.php" | jq -r '.result')
-
-if [[ -z "$1" ]]; then
-    echo "Usage: $0 [list|add|remove] args..."
-    exit 1
+if [[ "$1" == "list-hosts" ]]; then
+    curl -s -X POST -H 'Content-Type: application/json-rpc' \
+        -d "{\"jsonrpc\":\"2.0\",\"method\":\"host.get\",\"params\":{\"output\":[\"hostid\",\"host\"]},\"auth\":\"$API_LOGIN\",\"id\":2}" \
+        "$ZABBIX_URL/api_jsonrpc.php" | jq
+else
+    echo "Usage: $0 list-hosts"
 fi
-
-ACTION="$1"; shift
-
-case "$ACTION" in
-    list)
-        curl -s -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"host.get","params":{"output":["hostid","host","status"]},"auth":"'"$AUTH_TOKEN"'","id":1}' "$ZABBIX_URL/api_jsonrpc.php" | jq
-        ;;
-    add)
-        HOSTNAME="$1"
-        IP="$2"
-        curl -s -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"host.create","params":{"host":"'"$HOSTNAME"'","interfaces":[{"type":1,"main":1,"useip":1,"ip":"'"$IP"'","dns":"","port":"10050"}],"groups":[{"groupid":"2"}]},"auth":"'"$AUTH_TOKEN"'","id":1}' "$ZABBIX_URL/api_jsonrpc.php" | jq
-        ;;
-    remove)
-        HOSTID="$1"
-        curl -s -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"host.delete","params":["'"$HOSTID"'"],"auth":"'"$AUTH_TOKEN"'","id":1}' "$ZABBIX_URL/api_jsonrpc.php" | jq
-        ;;
-    *)
-        error "Unknown action"
-        ;;
-esac
