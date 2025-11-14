14eeb9ccc03bb99f294ac58e3c9099b8207f3bd2
Jonathan <79708886+Nix556@users.noreply.github.com>
2025-11-14 16:50:38 +0100

    Add system.sh for OS detection and system update

--- lib/system.sh
+++ lib/system.sh
@@ -1,19 +1,23 @@
+source colors.sh
+
 detect_os() {
     if [[ -f /etc/debian_version ]]; then
         OS_NAME="Debian"
         OS_VERSION=$(cut -d. -f1 /etc/debian_version)
         PM="apt"
     elif [[ -f /etc/lsb-release ]]; then
-        OS_NAME="Ubuntu"
+        OS_NAME=$(lsb_release -si)
         OS_VERSION=$(lsb_release -rs)
         PM="apt"
     else
         error "Unsupported OS"
         exit 1
     fi
+    success "Detected OS: $OS_NAME $OS_VERSION"
 }
 
 update_system() {
+    info "Updating package list..."
     if [[ $EUID -eq 0 ]]; then
         $PM update -y
     else
